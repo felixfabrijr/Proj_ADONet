d0359462a86a377ed69c4d98d01f6cb708c2d045
felix fabri <felix.fabrijr@gmail.com>
2023-09-10 13:07:17 +0100

    10/09/2023

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -31,13 +31,23 @@
             btnConnMySql = new Button();
             label1 = new Label();
             btnConnectionSqlServer = new Button();
+            btnListarSqlServer = new Button();
+            btnListarMySql = new Button();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            btnListarSqlServerDataSet = new Button();
+            btnListarMySqlDataSet = new Button();
+            dataGridView1 = new DataGridView();
+            dataGridView2 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
             SuspendLayout();
             // 
             // btnConnMySql
             // 
-            btnConnMySql.Location = new Point(34, 81);
+            btnConnMySql.Location = new Point(34, 48);
             btnConnMySql.Name = "btnConnMySql";
-            btnConnMySql.Size = new Size(154, 65);
+            btnConnMySql.Size = new Size(291, 65);
             btnConnMySql.TabIndex = 0;
             btnConnMySql.Text = "Conexao MySql";
             btnConnMySql.UseVisualStyleBackColor = true;
@@ -55,25 +65,119 @@
             // 
             // btnConnectionSqlServer
             // 
-            btnConnectionSqlServer.Location = new Point(221, 81);
+            btnConnectionSqlServer.Location = new Point(384, 48);
             btnConnectionSqlServer.Name = "btnConnectionSqlServer";
-            btnConnectionSqlServer.Size = new Size(154, 65);
+            btnConnectionSqlServer.Size = new Size(291, 65);
             btnConnectionSqlServer.TabIndex = 2;
             btnConnectionSqlServer.Text = "Conexao Sql Server";
             btnConnectionSqlServer.UseVisualStyleBackColor = true;
+            btnConnectionSqlServer.Click += btnConnectionSqlServer_Click;
+            // 
+            // btnListarSqlServer
+            // 
+            btnListarSqlServer.Location = new Point(384, 119);
+            btnListarSqlServer.Name = "btnListarSqlServer";
+            btnListarSqlServer.Size = new Size(291, 65);
+            btnListarSqlServer.TabIndex = 4;
+            btnListarSqlServer.Text = "Listar Sql Server [DataReader]";
+            btnListarSqlServer.UseVisualStyleBackColor = true;
+            btnListarSqlServer.Click += btnListarSqlServer_Click;
+            // 
+            // btnListarMySql
+            // 
+            btnListarMySql.Location = new Point(34, 119);
+            btnListarMySql.Name = "btnListarMySql";
+            btnListarMySql.Size = new Size(291, 65);
+            btnListarMySql.TabIndex = 3;
+            btnListarMySql.Text = "Listar MySql [DataReader]";
+            btnListarMySql.UseVisualStyleBackColor = true;
+            btnListarMySql.Click += btnListarMySql_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(34, 190);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(291, 254);
+            listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 25;
+            listBox2.Location = new Point(384, 191);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(291, 254);
+            listBox2.TabIndex = 6;
+            // 
+            // btnListarSqlServerDataSet
+            // 
+            btnListarSqlServerDataSet.Location = new Point(1064, 119);
+            btnListarSqlServerDataSet.Name = "btnListarSqlServerDataSet";
+            btnListarSqlServerDataSet.Size = new Size(291, 65);
+            btnListarSqlServerDataSet.TabIndex = 8;
+            btnListarSqlServerDataSet.Text = "Listar Sql Server [DataSet]";
+            btnListarSqlServerDataSet.UseVisualStyleBackColor = true;
+            btnListarSqlServerDataSet.Click += btnListarSqlServerDataSet_Click;
+            // 
+            // btnListarMySqlDataSet
+            // 
+            btnListarMySqlDataSet.Location = new Point(714, 119);
+            btnListarMySqlDataSet.Name = "btnListarMySqlDataSet";
+            btnListarMySqlDataSet.Size = new Size(291, 65);
+            btnListarMySqlDataSet.TabIndex = 7;
+            btnListarMySqlDataSet.Text = "Listar MySql [DataSet]";
+            btnListarMySqlDataSet.UseVisualStyleBackColor = true;
+            btnListarMySqlDataSet.Click += btnListarMySqlDataSet_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(714, 192);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.RowTemplate.Height = 33;
+            dataGridView1.Size = new Size(291, 253);
+            dataGridView1.TabIndex = 9;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Location = new Point(1064, 191);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.RowHeadersWidth = 62;
+            dataGridView2.RowTemplate.Height = 33;
+            dataGridView2.Size = new Size(291, 253);
+            dataGridView2.TabIndex = 10;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1395, 558);
+            Controls.Add(dataGridView2);
+            Controls.Add(dataGridView1);
+            Controls.Add(btnListarSqlServerDataSet);
+            Controls.Add(btnListarMySqlDataSet);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(btnListarSqlServer);
+            Controls.Add(btnListarMySql);
             Controls.Add(btnConnectionSqlServer);
             Controls.Add(label1);
             Controls.Add(btnConnMySql);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
             ResumeLayout(false);
             PerformLayout();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
@@ -81,5 +185,13 @@
         private Button btnConnMySql;
         private Label label1;
         private Button btnConnectionSqlServer;
+        private Button btnListarSqlServer;
+        private Button btnListarMySql;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Button btnListarSqlServerDataSet;
+        private Button btnListarMySqlDataSet;
+        private DataGridView dataGridView1;
+        private DataGridView dataGridView2;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,4 +1,8 @@
+using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic.ApplicationServices;
 using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 
 namespace WinFormsApp1
 {
@@ -9,14 +13,111 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
+        //private void label1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
         private void btnConnMySql_Click(object sender, EventArgs e)
         {
             MySqlConnection conn = new MySqlConnection();
 
-            conn.ConnectionString = "";
+            conn.ConnectionString = "Server=localhost;Database=Cliente01;Uid=root;Pwd=senha@123;";
             conn.Open();
 
-            MessageBox.Show("Abri conexao com Mysql")
+            MessageBox.Show("Abre conexao MySql");
+
+            conn.Close();
+        }
+
+        private void btnConnectionSqlServer_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+
+            conn.ConnectionString = "Data Source=localhost;User ID=sa;Password=senha@123;Connect Timeout = 30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            conn.Open();
+
+            MessageBox.Show("Abre conexao SqlServer");
+
+            conn.Close();
+        }
+
+        private void btnListarMySql_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conn = new MySqlConnection();
+
+            string connection = @"Server=localhost;Database=Cliente01;Uid=root;Pwd=senha@123;";
+
+            conn.ConnectionString = connection;
+            conn.Open();
+
+            MySqlCommand conm = new MySqlCommand();
+
+
+            conm.CommandType = System.Data.CommandType.Text;
+            conm.CommandText = "select * from Produtos";
+            conm.Connection = conn;
+
+            MySqlDataReader reader = conm.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string nome = reader.GetString(1);
+                string cor = reader.IsDBNull(2) ? string.Empty : reader.GetString(2);
+
+                listBox1.Items.Add($"{reader.GetInt32(0)}, {nome}, {cor}");
+            }
+
+            conn.Close();
+        }
+
+        private void btnListarSqlServer_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection();
+
+            string connection = @"Data Source=localhost;User ID=sa;Password=senha@123;Connect Timeout = 30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Initial Catalog=Cliente01";
+
+            conn.ConnectionString = connection;
+            conn.Open();
+
+            SqlCommand conm = new SqlCommand();
+
+
+            conm.CommandType = System.Data.CommandType.Text;
+            conm.CommandText = "select * from Produtos";
+            conm.Connection = conn;
+
+            SqlDataReader reader = conm.ExecuteReader();
+
+            while (reader.Read())
+            {
+                string nome = reader.GetString(1);
+                string cor = reader.IsDBNull(2) ? string.Empty : reader.GetString(2);
+
+                listBox2.Items.Add($"{reader.GetInt32(0)}, {nome}, {cor}");
+            }
+
+            conn.Close();
+        }
+
+        private void btnListarMySqlDataSet_Click(object sender, EventArgs e)
+        {
+            string connection = @"Server=localhost;Database=Cliente01;Uid=root;Pwd=senha@123;";
+            MySqlDataAdapter da = new MySqlDataAdapter("select * from Produtos", connection);
+            DataSet dataSet = new DataSet();
+            da.Fill(dataSet);
+            dataGridView1.DataSource = dataSet.Tables[0];
+        }
+
+        private void btnListarSqlServerDataSet_Click(object sender, EventArgs e)
+        {
+           
+            string connection = @"Data Source=localhost;User ID=sa;Password=senha@123;Connect Timeout = 30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False;Initial Catalog=Cliente01";
+            SqlDataAdapter da = new SqlDataAdapter("select * from Produtos", connection);
+            DataSet dataSet = new DataSet();
+            da.Fill(dataSet);
+            dataGridView2.DataSource = dataSet.Tables[0];
+
         }
     }
 }
